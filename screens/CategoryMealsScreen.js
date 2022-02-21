896a6cff965fb22b5bd39abb7c32331c66e699e2
KronosGR <geo.elgeo@gmail.com>
2022-02-21 12:09:26 +0100

    added dynamic navigation option

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Platform } from 'react-native';
+import Colors from '../constants/Colors';
 import { CATEGORIES } from '../data/dummy-data';
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam('categoryId');
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
-  
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
@@ -24,6 +25,19 @@ const CategoryMealsScreen = (props) => {
       />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.Os === 'android' ? Colors.primaryColor : 'white',
+    },
+    headerTintColor: Platform.Os === 'android' ? 'white' : Colors.primaryColor,
+  };
 };
 
 const styles = StyleSheet.create({
