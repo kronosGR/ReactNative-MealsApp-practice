63fda2143acbe7b4bdea97bc460349d8a111097c
KronosGR <geo.elgeo@gmail.com>
2022-02-23 10:25:39 +0100

    pass data MealDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { HeaderTitle } from 'react-navigation-stack';
+import { MEALS } from '../data/dummy-data';
 
 const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>The MealDetail Screen!</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title='Go Back to Categories'
         onPress={() => {
@@ -13,6 +17,15 @@ const MealDetailScreen = (props) => {
       />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return {
+    headerTitle: selectedMeal.title,
+  };
 };
 
 const styles = StyleSheet.create({
