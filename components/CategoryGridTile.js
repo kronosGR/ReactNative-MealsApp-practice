2193df32779e5cbd5aec51f001f15a2e193ca72d
KronosGR <geo.elgeo@gmail.com>
2022-02-24 08:51:11 +0100

    fixed shadows

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -33,7 +33,8 @@ const styles = StyleSheet.create({
     margin: 10,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+    elevation: 3,
   },
   container: {
     flex: 1,
@@ -42,7 +43,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
