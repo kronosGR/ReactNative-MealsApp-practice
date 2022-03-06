a292090f411e24aa1200150793277847bbcb1d69
KronosGR <geo.elgeo@gmail.com>
2022-03-06 06:08:48 +0100

    redux installation and setup

--- App.js
+++ App.js
@@ -2,10 +2,19 @@ import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
-import MealsNavigator from './navigation/MealsNavigator';
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducers from './store/reducers/meals';
 
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducers,
+});
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -30,10 +39,8 @@ export default function App() {
   }
 
   return (
-    <MealsNavigator>
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    </MealsNavigator>
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
