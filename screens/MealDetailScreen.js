1f8f7e0096063743018e3f656fe673733b21e621
KronosGR <geo.elgeo@gmail.com>
2022-03-07 06:40:04 +0100

    switching the fav icon

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -18,6 +18,10 @@ const ListItem = (props) => {
 
 const MealDetailScreen = (props) => {
   const availableMeals = useSelector((state) => state.meals.meals);
+  const currentMealIsFavorite = useSelector((state) =>
+    state.meals.favoriteMeals.some((meal) => (meal.id = mealId))
+  );
+
   const mealId = props.navigation.getParam('mealId');
 
   const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
@@ -30,7 +34,11 @@ const MealDetailScreen = (props) => {
   useEffect(() => {
     // props.navigation.setParams({ mealTitle: selectedMeal.title });
     props.navigation.setParams({ toggleFav: toggleFavoriteHandler });
-  }, [selectedMeal]);
+  }, [toggleFavoriteHandler]);
+
+  useEffect(() => {
+    props.navigation.setParams({ isFav: currentMealIsFavorite });
+  }, [currentMealIsFavorite]);
 
   return (
     <ScrollView>
@@ -56,13 +64,17 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   // const mealId = navigationData.navigation.getParam('mealId');
   const mealTitle = navigationData.navigation.getParam('mealTitle');
   const toggleFavorite = navigationData.navigation.getParam('toggleFav');
-  // const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const isFavorite = navigationData.navigation.getParam('isFav');
 
   return {
     headerTitle: mealTitle,
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title='Favorite' iconName='ios-star' onPress={toggleFavorite} />
+        <Item
+          title='Favorite'
+          iconName={isFavorite ? 'ios-star' : 'ios-star-outline'}
+          onPress={toggleFavorite}
+        />
       </HeaderButtons>
     ),
   };
